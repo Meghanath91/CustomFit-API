c7f688605db5ec8258176e64fd17fcd217976a96
Meghanath91 <meghanath.balaji@gmail.com>
2020-03-17 21:01:48 -0400

    cors installed and routes cleaned up

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../db/index");
-
 pool.connect();
 
 router.get("/trainers", (req, res) => {
@@ -12,6 +11,19 @@ router.get("/trainers", (req, res) => {
       res.json(result.rows);
     });
 });
+
+// router.get("/trainers",(req,res)=>{
+//   const {name,email,password,phone,about,avatar,experience} =req.body
+//   pool.query(`
+//   INSERT INTO trainers (name, email, password, phone, about, avatar, experience) VALUES ($1::text, $2::text, $3::text, $4::text, $5::text, $6::text, $7::text);
+//   `,[name, email, password, phone, about, avatar, experience]
+//   )
+//   .then(() => {
+//     response.json(`database:trainer ${request.params.id}created`);
+//   })
+//   .catch(error => console.log(error));
+
+// })
 
 router.get("/students", (req, res) => {
   pool.query(`
@@ -40,6 +52,27 @@ router.get("/exercises", (req, res) => {
   })
 });
 
+router.get("/exercises/:id",(req,res)=>{
+  const id = parseInt(req.params.id)
+  pool.query(`
+  SELECT * FROM exercises WHERE id = $1;
+  `,[id])
+  .then(result=>{
+    res.json(result.rows)
+  })
+})
+// router.delete("/exercises/:id",(req,res)=>{
+//   const id = parseInt(req.params.id)
+//   pool.query(`
+//   DELETE FROM exercises WHERE id = $1
+//   `,[id])
+//   .then(() => {
+//     response.json(`database:exercise ${id}deleted`);
+//   })
+//   .catch(error => console.log(error));
+// }
+// )
+
 router.get("/workout_exercises", (req, res) => {
   pool.query(`
   SELECT * FROM workout_exercises;
@@ -58,11 +91,30 @@ router.get("/history", (req, res) => {
   })
 });
 
+router.post("/history",(req,res) => {
+  const {
+    workout_exercise_id,
+    feedback_text,
+    feedback_video
 
-router.get("")
+  } = req.body;
+  pool.query(`
+  INSERT INTO history (workout_exercise_id, feedback_text, feedback_video) VALUES ($1::integer, $2::text, $3::text);
 
-router.get("/", (req, res) => {
-  res.send("Hello world");
+  `,[workout_exercise_id,feedback_text,feedback_video]
+  )
+  .then(() => {
+    response.json(`database:history ${request.params.id}created`);
+  })
+  .catch(error => console.log(error));
+
 });
+
+
+
+
+// router.get("/", (req, res) => {
+//   res.send("Hello world");
+// });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const route = require('./routes/index')
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -10,14 +11,9 @@ const port = process.env.PORT || 8080;
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/', route)
-app.use('/trainers', route)
-app.use('/students',route)
-app.use('/exercises',route)
-app.use('/custom_plans',route)
-app.use('/workout_exercises',route)
-app.use('/history',route)
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
