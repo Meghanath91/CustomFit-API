4eecedef4412fefc2af146c4c9d891eddc9aaf8b
Shivam Patel <patelsd96@gmail.com>
2020-03-25 15:31:35 -0400

    added exercises, studnets, trainers

--- db/seeds/01_trainers.sql
+++ db/seeds/01_trainers.sql
@@ -1,4 +1,11 @@
 INSERT INTO trainers (name, email, password, phone, about, avatar, experience) VALUES ('Shivam Patel', 'ShivamPatel@lighthouse.com', 'password', 9051818282, 'Weightlifing trainer', 'https://miro.medium.com/max/1800/1*DcXbEjyiKj3OseCrg9ftNw.jpeg', '5 years experience');
 
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience) VALUES ('Parth Desai', 'Parth@lighthouse.com', 'password', 9051234321, 'Weightloss trainer', 'https://content.active.com/Assets/Active.com+Content+Site+Digital+Assets/Fitness/Galleries/10+Fitness+Tips/Muscle-confusion-is-the-only-way-to-go..jpg', '10 years experience');
 
+
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience) VALUES ('Meghanath', 'Meghanath@lighthouse.com', 'password', 9054545658, 'Mindfulness trainer', 'https://static.standard.co.uk/s3fs-public/thumbnails/image/2019/02/13/14/indian-yogi-yogi-madhav-727510-unsplash.jpg?w968', '15 years experience');
+
+
+
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience) VALUES ('Jane Doe', 'Jane@lighthouse.com', 'password', 9057895485, 'Running trainer', 'https://hips.hearstapps.com/assets.runnersworld.co.uk/i/15828/running_hat.jpg', '15 years experience');
 
--- db/seeds/04_exercises.sql
+++ db/seeds/04_exercises.sql
@@ -11,3 +11,18 @@ INSERT INTO exercises (name, explanation, body_part, type, thumbnail_photo_url, 
 
 INSERT INTO exercises (name, explanation, body_part, type, thumbnail_photo_url, content_video) VALUES ('Squat', '1. bend your legs', 'ABS', 'BodyWeight', '/pictures/squat.gif', 'https://www.youtube.com/watch?v=8jyhJ6TiUPA');
 
+INSERT INTO exercises (name, explanation, body_part, type, thumbnail_photo_url, content_video) VALUES ('Leg Raises', '1. Lie down and connect feet. 2. Tighten core and keep legs straight and raise them up', 'ABS', 'BodyWeight', '/pictures/legRaise.gif', 'https://i.imgur.com/JtgnFH1.gif');
+
+INSERT INTO exercises (name, explanation, body_part, type, thumbnail_photo_url, content_video) VALUES ('Russian Twist', '1. bend your legs, and bring them off the ground. 2. Balance on your glutes and twist', 'ABS', 'BodyWeight', '/pictures/russianTwist.gif', 'https://i.imgur.com/PG6vgpl.gif');
+
+
+INSERT INTO exercises (name, explanation, body_part, type, thumbnail_photo_url, content_video) VALUES ('Side Jack-Knife', '1. Lie down on your side, flex obliques', 'ABS', 'BodyWeight', '/pictures/sideJKnife.gif', 'https://i.imgur.com/HjyLvkX.gif');
+
+
+INSERT INTO exercises (name, explanation, body_part, type, thumbnail_photo_url, content_video) VALUES ('Plank', '1. Lie down on your chest and pick yourself up into position as shown in the image. 2. Keep core tight', 'ABS', 'BodyWeight', '/pictures/plank.gif', 'https://i.imgur.com/OrUga8Z.gif');
+
+
+INSERT INTO exercises (name, explanation, body_part, type, thumbnail_photo_url, content_video) VALUES ('Flutter Kicks', '1. Lie down on your back. 2. Lift legs off floor slightly and flutter straight legs', 'ABS', 'BodyWeight', '/pictures/flutterKick.gif', 'https://i.imgur.com/eotOxLd.gif');
+
+
+
