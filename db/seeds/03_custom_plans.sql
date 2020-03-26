0f5b5a8c0153a94f4d2aa785927c16af42cdf477
Meghanath91 <meghanath.balaji@gmail.com>
2020-03-25 22:22:41 -0400

    db updated

--- db/seeds/03_custom_plans.sql
+++ db/seeds/03_custom_plans.sql
@@ -1,2 +1,5 @@
 INSERT INTO custom_plans (student_id, trainer_id, title, description, difficulty, type) VALUES (1, 1, 'Ripped Workout Plan', 'THis plan is made to help student1 get bigger and stronger', 'beginner', 'WL');
 
+INSERT INTO custom_plans (student_id, trainer_id, title, description, difficulty, type) VALUES (2, 1, 'Ripped Workout Plan', 'THis plan is made to help student2 get bigger and stronger', 'beginner', 'WL');
+
+INSERT INTO custom_plans (student_id, trainer_id, title, description, difficulty, type) VALUES (3, 1, 'Ripped Workout Plan', 'THis plan is made to help student3 get bigger and stronger', 'beginner', 'WL');
