673369676b94360ecdc6cdfb6b084bd91f222c3b
Shivam Patel <patelsd96@gmail.com>
2020-03-30 23:41:04 -0400

    added trainers and ocus

--- db/seeds/01_trainers.sql
+++ db/seeds/01_trainers.sql
@@ -1,42 +1,42 @@
-INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Shivam Patel', 'ShivamPatel@lighthouse.com', 'password', 9051818282, 'Weightlifing trainer', '/pictures/trainer1.png', '5 years experience', 'weightgain');
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Shivam Patel', 'ShivamPatel@gmail.com', 'password', 9051818282, 'Weightlifing trainer', '/pictures/trainer1.png', '5 years experience', 'weightgain');
 
-INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Parth Desai', 'Parth@lighthouse.com', 'password', 9051234321, 'Weightloss trainer', '/pictures/trainer2.png', '10 years experience', 'weightloss');
-
-
-INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Meghanath', 'Meghanath@lighthouse.com', 'password', 9054545658, 'Mindfulness trainer', '/pictures/trainer3.png', '15 years experience', 'overall');
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Parth Desai', 'Parth@gmail.com', 'password', 9051234321, 'Weightloss trainer', '/pictures/trainer2.png', '10 years experience', 'weightloss');
 
 
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Meghanath', 'Meghanath@gmail.com', 'password', 9054545658, 'Mindfulness trainer', '/pictures/trainer3.png', '15 years experience', 'overall');
 
-INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Jane Doe', 'Jane@lighthouse.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer4.png', '8 years experience', 'yoga');
 
 
-INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Charley Whittle', 'Jane@lighthouse.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer5.png', '3 years experience', 'mediatation');
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Jane Doe', 'Jane@gmail.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer4.png', '8 years experience', 'yoga');
 
-INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Maisy Ferguson', 'Jane@lighthouse.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer6.png', '4 years experience', 'mediatation');
 
-INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Josie Barnes', 'Jane@lighthouse.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer7.png', '2 years experience', 'weightloss');
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Charley Whittle', 'Charley@gmail.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer5.png', '3 years experience', 'mediatation');
 
-INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Leona Yang', 'Jane@lighthouse.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer8.png', '1 years experience', 'weightgain');
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Maisy Ferguson', 'Maisy@gmail.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer6.png', '4 years experience', 'mediatation');
 
-INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Hana Wilkins', 'Jane@lighthouse.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer9.png', '9 years experience', 'weightloss');
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Josie Barnes', 'Josie@gmail.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer7.png', '2 years experience', 'weightloss');
 
-INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Jacob Watson', 'Jane@lighthouse.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer10.png', '10 years experience', 'weightgain');
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Leona Yang', 'Leona@gmail.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer8.png', '1 years experience', 'weightgain');
 
-INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Seth Gordon', 'Jane@lighthouse.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer4.png', '20 years experience', 'overall');
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Hana Wilkins', 'Hana@gmail.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer9.png', '9 years experience', 'weightloss');
 
-INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Shawn Banks', 'Jane@lighthouse.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer11.png', '30 years experience', 'overall');
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Jacob Watson', 'Jacob@gmail.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer10.png', '10 years experience', 'weightgain');
 
-INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Daniel May', 'Jane@lighthouse.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer4.png', '12 years experience', 'yoga');
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Seth Gordon', 'Seth@gmail.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer4.png', '20 years experience', 'overall');
 
-INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Jasmeet Munday', 'Jane@lighthouse.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer12.png', '14 years experience', 'weightloss');
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Shawn Banks', 'Shawn@gmail.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer11.png', '30 years experience', 'overall');
 
-INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Alfie Carter', 'Jane@lighthouse.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer13.png', '9 years experience', 'weightloss');
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Daniel May', 'Daniel@gmail.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer4.png', '12 years experience', 'yoga');
 
-INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Otto Sparks', 'Jane@lighthouse.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer14.png', '4 years experience', 'mediatation');
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Jasmeet Munday', 'Jasmeet@gmail.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer12.png', '14 years experience', 'weightloss');
 
-INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Michaela  Fuller', 'Jane@lighthouse.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer15.png', '5 years experience', 'overall');
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Alfie Carter', 'Alfie@gmail.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer13.png', '9 years experience', 'weightloss');
 
-INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Katy Baldwin', 'Jane@lighthouse.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer16.png', '13 years experience', 'yoga');
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Otto Sparks', 'Otto@gmail.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer14.png', '4 years experience', 'mediatation');
 
-INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Veronica Wells', 'Jane@lighthouse.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer17.png', '18 years experience', 'yoga');
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Michaela  Fuller', 'Michaela@gmail.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer15.png', '5 years experience', 'overall');
+
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Katy Baldwin', 'Katy@gmail.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer16.png', '13 years experience', 'yoga');
+
+INSERT INTO trainers (name, email, password, phone, about, avatar, experience, expertise) VALUES ('Veronica Wells', 'Veronica@gmail.com', 'password', 9057895485, 'Running trainer', '/pictures/trainer17.png', '18 years experience', 'yoga');
 
--- db/seeds/02_students.sql
+++ db/seeds/02_students.sql
@@ -1,16 +1,39 @@
-INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight) VALUES ('Student 1', 'Student1@lighthouse.com', 'password', 9054548778, 21, '/pictures/student1.png', 'I want to be become physically stronger', 150, 200);
+INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight, focus) VALUES ('Jemima Mccarthy', 'JemimaMccarthy@gmail.com', 'password', 9054548778, 21, '/pictures/student1.png', 'I want to be become physically stronger', 150, 200, 'weightloss');
 
-INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight) VALUES ('Student 2', 'Student2@lighthouse.com', 'password', 9054745212, 22, '/pictures/student2.png', 'I want to be more mindful', 110, 100);
+INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight, focus) VALUES ('Kayleigh Hunt', 'KayleighHunt@gmail.com', 'password', 9054745212, 22, '/pictures/student4.png', 'I want to be more mindful', 110, 100, 'weightgain');
 
-INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight) VALUES ('Student 3', 'Student3@lighthouse.com', 'password', 9052563543, 23, '/pictures/student3.png', 'I want to be fit', 120, 123);
+INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight, focus) VALUES ('Lisa Burke', 'LisaBurke@gmail.com', 'password', 9052563543, 23, '/pictures/student23.png', 'I want to be fit', 120, 123, 'overall');
 
-INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight) VALUES ('Student 4', 'Student4@lighthouse.com', 'password', 9057854965, 24, '/pictures/student4.png', 'I want to work on my cardio', 125, 220);
+INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight, focus) VALUES ('Thea Berry', 'TheaBerry@gmail.com', 'password', 9057854965, 24, '/pictures/student22.png', 'I want to work on my cardio', 125, 220, 'yoga');
 
-INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight) VALUES ('Student 5', 'Student5@lighthouse.com', 'password', 9052314565, 25, '/pictures/student5.png', 'I need to lower my cholesterol', 160, 210);
+INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight, focus) VALUES ('Nannie Mcbride', 'NannieMcbride@gmail.com', 'password', 9052314565, 25, '/pictures/student19.png', 'I need to lower my cholesterol', 160, 210, 'mediatation');
 
-INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight) VALUES ('Student 6', 'Student6@lighthouse.com', 'password', 9053265854, 26, '/pictures/student6.png', 'I like working out, but I am running out of ideas', 130, 220);
+INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight, focus) VALUES ('Chelsea Fleming', 'ChelseaFleming@gmail.com', 'password', 9053265854, 26, '/pictures/student20.png', 'I like working out, but I am running out of ideas', 130, 220, 'weightloss');
 
-INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight) VALUES ('Student 7', 'Student7@lighthouse.com', 'password', 9056965236, 27, '/pictures/student7.png', 'I want to be become lean', 170, 187);
+INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight, focus) VALUES ('Isabella Abbott', 'Isabella@gmail.com', 'password', 9056965236, 28, '/pictures/student24.png', 'I want to be become lean', 170, 187, 'weightgain');
+
+INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight, focus) VALUES ('Orla Walton', 'Orla@gmail.com', 'password', 9056965236, 29, '/pictures/student8.png', 'I want to be become healthy', 170, 187, 'overall');
+
+
+INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight, focus) VALUES ('Beatrice Rivera', 'Beatrice@gmail.com', 'password', 9056965236, 30, '/pictures/student9.png', 'I want to be become a model', 170, 187, 'weightloss');
+
+INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight, focus) VALUES ('Jessie Johnston', 'Jessie@gmail.com', 'password', 9056965236, 47, '/pictures/student10.png', 'I want to be become fast', 170, 187, 'weightgain');
+
+INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight, focus) VALUES ('Kane Bennett', 'Kane@gmail.com', 'password', 9056965236, 67, '/pictures/student11.png', 'I want to be become a hero', 170, 187, 'mediatation');
+
+INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight, focus) VALUES ('Lee Cooper', 'Lee@gmail.com', 'password', 9056965236, 47, '/pictures/student12.png', 'I want to be become a world champion', 170, 187, 'weightgain');
+
+INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight, focus) VALUES ('Zack Jordan', 'Zack@gmail.com', 'password', 9056965236, 23, '/pictures/student13.png', 'I want to be become skinny', 170, 187, 'overall');
+
+INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight, focus) VALUES ('Calvin Gibson', 'Calvin@gmail.com', 'password', 9056965236, 22, '/pictures/student14.png', 'I want to be become lean', 170, 187, 'weightloss');
+
+INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight, focus) VALUES ('Ralph Lopez', 'Ralph@gmail.com', 'password', 9056965236, 26, '/pictures/student15.png', 'I want to be become lean', 170, 187, 'overall');
+
+INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight, focus) VALUES ('Yusuf King', 'Yusuf@gmail.com', 'password', 9056965236, 24, '/pictures/student16.png', 'I want to be become lean', 170, 187, 'yoga');
+
+INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight, focus) VALUES ('Eden Harrison', 'Eden@gmail.com', 'password', 9056965236, 33, '/pictures/student17.png', 'I want to be become lean', 170, 187, 'weightloss');
+
+INSERT INTO students (name, email, password, phone, age, avatar, goal, height, weight, focus) VALUES ('Theo Morris', 'Theo@gmail.com', 'password', 9056965236, 34, '/pictures/student18.png', 'I want to be become lean', 170, 187, 'mediatation');
 
 
 
