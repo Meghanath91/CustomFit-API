1d09ce099820fac102d3c5c749833a62492f14c0
Meghanath91 <meghanath.balaji@gmail.com>
2020-04-14 20:51:34 -0400

    client updated

--- db/index.js
+++ db/index.js
@@ -9,14 +9,3 @@ const pool = new Pool({
   port: 5432
 });
 module.exports = pool;
-
-
-const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || ""
-});
-
-client
-  .connect()
-  .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
-
-module.exports = client;
