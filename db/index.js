e85adef687937149b005b4f23a7c0b21e11ab6e2
Meghanath91 <meghanath.balaji@gmail.com>
2020-04-21 15:17:29 -0400

    pool changed

--- db/index.js
+++ db/index.js
@@ -2,10 +2,7 @@ const { Pool } = require("pg");
 
 //****connect database using node-postgress****//
 const pool = new Pool({
-  user: "labber",
-  password: "labber",
-  host: "localhost",
-  database: "final",
-  port: 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl:true
 });
 module.exports = pool;
